b9aabe6fd2a87b9cf98714ec8afac1e0938b0cbd
Damian Demasi <damian.demasi.1@gmail.com>
2021-07-04 17:44:22 +0930

    Input tasks

--- todo-app/js/index.js
+++ todo-app/js/index.js
@@ -22,14 +22,22 @@ const itemsList = [
 
 // SECTION Function definitions
 
-const loadStartingTasks = () => {
-    const startingTasks = Array.from(document.querySelectorAll(".todo--tasks-list--item--description"));
-
-    startingTasks.forEach((element, i) => {
-        element.textContent = itemsList[i].task;
-
+const displayTasks = () => {
+    const list = document.getElementById('todo--tasks-list--items-list');
+    // Clear list
+    const li = document.querySelectorAll('li');
+    li.forEach(element => {
+        element.remove();
+    })
+    // Display list
+    itemsList.reverse().forEach((element, i) => {
+        list.insertAdjacentHTML('afterbegin', `<li class="todo--tasks-list--item">
+            <span class="todo--tasks-list--item--checkbox"></span>
+            <span class="todo--tasks-list--item--description">${itemsList[i].task}</span>
+        </li>`);
     });
-}
+    itemsList.reverse(); // Back to normal FIXME
+};
 
 const lineThroughText = (i) => {
     const itemToLineThrough = Array.from(document.querySelectorAll('.todo--tasks-list--item--description'));
@@ -38,7 +46,7 @@ const lineThroughText = (i) => {
 
 // SECTION ToDo list behaviour
 
-loadStartingTasks();
+displayTasks();
 
 const checkBox = Array.from(document.querySelectorAll('.todo--tasks-list--item--checkbox'));
 
@@ -55,6 +63,7 @@ checkBox.forEach((element, i) => {
 const addTaskButton = document.querySelector('#add-task');
 const modal = document.getElementById("myModal");
 const span = document.getElementsByClassName("close")[0];
+const add = document.getElementById('btn-add-task');
 
 addTaskButton.onclick = function () {
     modal.style.display = "block";
@@ -72,3 +81,19 @@ window.onclick = function (event) {
     if (event.target === modal)
         modal.style.display = "none";
 }
+
+add.addEventListener('click', function () {
+    const newTask = {};
+    if (document.getElementById('input-task').value !== '') {
+        newTask.task = document.getElementById('input-task').value;
+        newTask.dueDate = '10/10/2021'; // TODO get actual dueDate
+        newTask.completed = false;
+        itemsList.unshift(newTask);
+        displayTasks();
+        modal.style.display = "none";
+        document.getElementById('input-task').value = '';
+    } else {
+        document.getElementById('input-task').style.border = '1px solid red'; // Fade this down TODO FIXME
+        document.getElementById('input-task').focus();
+    }
+});
