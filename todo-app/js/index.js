04e616ce723e9a7567bec7b90f0f4d862007f325
Damian Demasi <damian.demasi.1@gmail.com>
2021-07-04 16:31:22 +0930

    Check boxes and modal

--- todo-app/js/index.js
+++ todo-app/js/index.js
@@ -20,13 +20,53 @@ const itemsList = [
     },
 ];
 
+// SECTION Function definitions
+
 const loadStartingTasks = () => {
     const startingTasks = Array.from(document.querySelectorAll(".todo--tasks-list--item--description"));
 
     startingTasks.forEach((element, i) => {
         element.textContent = itemsList[i].task;
+
     });
 }
 
+const lineThroughText = (i) => {
+    const itemToLineThrough = Array.from(document.querySelectorAll('.todo--tasks-list--item--description'));
+    itemToLineThrough[i].classList.toggle('todo--tasks-list--item--description--checked');
+};
+
+// SECTION ToDo list behaviour
+
 loadStartingTasks();
 
+const checkBox = Array.from(document.querySelectorAll('.todo--tasks-list--item--checkbox'));
+
+checkBox.forEach((element, i) => {
+    element.addEventListener('click', function () {
+        element.classList.toggle('todo--tasks-list--item--checkbox--checked');
+        itemsList[i].completed = !itemsList[i].completed;
+        lineThroughText(i);
+    });
+})
+
+// SECTION Add new task
+
+const addTaskButton = document.querySelector('#add-task');
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
+
+addTaskButton.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target === modal)
+        modal.style.display = "none";
+}
